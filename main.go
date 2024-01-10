ffb5d3dadfb2c7292f8e118b12f31f2dcf136a26
도페 <152076496+dfpapyrus@users.noreply.github.com>
2024-01-11 00:22:44 +0900

    Edit

---
+++ main.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"web/myapp"
+)
+
+type User struct {
+	FirstName string
+	LastName  string
+	Email     string
+	CreatedAt time.Time
+}
+
+type fooHandler struct{}
+
+func (f *fooHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	user := new(User)
+	err := json.NewDecoder(r.Body).Decode(user)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		fmt.Fprint(w, "Bad Request: ", err)
+		return
+	}
+}
+
+func barHandler(w http.ResponseWriter, r *http.Request) {
+	name := r.URL.Query().Get("name")
+	if name == "" {
+		name = "World"
+	}
+	fmt.Fprintf(w, "Hello %s!", name)
+}
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w, "Hello World")
+	})
+
+	mux.HandleFunc("/bar", barHandler)
+
+	mux.Handle("/foo", &fooHandler{})
+
+	http.ListenAndServe(":3000", myapp.NewHttpHandler())
+}
